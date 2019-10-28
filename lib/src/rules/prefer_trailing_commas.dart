1f011c9ec07e3ec5669d45b675743a4baa3caf81
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2019-10-28 16:23:49 +0100

    update

--- lib/src/rules/prefer_trailing_commas.dart
+++ lib/src/rules/prefer_trailing_commas.dart
@@ -6,7 +6,6 @@ import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/token.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:linter/src/analyzer.dart';
-import 'package:linter/src/util/flutter_utils.dart';
 
 const _desc = r'Perfer trailing commas.';
 
@@ -47,16 +46,10 @@ class PreferTrailingCommas extends LintRule implements NodeLintRule {
       [LinterContext context]) {
     final visitor = _Visitor(this);
     registry.addCompilationUnit(this, visitor);
-    registry.addFormalParameterList(this, visitor);
-    registry.addFunctionExpressionInvocation(this, visitor);
-    registry.addInstanceCreationExpression(this, visitor);
-    registry.addListLiteral(this, visitor);
-    registry.addSetOrMapLiteral(this, visitor);
-    registry.addMethodInvocation(this, visitor);
   }
 }
 
-class _Visitor extends SimpleAstVisitor<void> {
+class _Visitor extends GeneralizingAstVisitor<void> {
   _Visitor(this.rule);
 
   final LintRule rule;
@@ -66,56 +59,35 @@ class _Visitor extends SimpleAstVisitor<void> {
   @override
   void visitCompilationUnit(CompilationUnit node) {
     lineInfo = node.lineInfo;
+    super.visitCompilationUnit(node);
+  }
+
+  @override
+  void visitArgumentList(ArgumentList node) {
+    _visitNodeList(node.arguments);
+    super.visitArgumentList(node);
   }
 
   @override
   void visitFormalParameterList(FormalParameterList node) {
     _visitNodeList(node.parameters);
-  }
-
-  @override
-  void visitFunctionExpressionInvocation(FunctionExpressionInvocation node) {
-    if (node.argumentList.arguments.length > 1) {
-      _visitNodeList(node.argumentList.arguments);
-    }
-  }
-
-  @override
-  void visitInstanceCreationExpression(InstanceCreationExpression node) {
-    if (node.argumentList.arguments.length > 1) {
-      _visitNodeList(node.argumentList.arguments);
-    } else if (node.argumentList.arguments.length == 1) {
-      var arg = node.argumentList.arguments.first;
-      if (arg is NamedExpression) arg = (arg as NamedExpression).expression;
-      if (isWidgetType(node.staticType) && isWidgetProperty(arg.staticType) ||
-          arg is InstanceCreationExpression &&
-              arg.argumentList.arguments.isNotEmpty &&
-              arg.argumentList.arguments.last.endToken?.next?.type ==
-                  TokenType.COMMA) {
-        _visitNodeList(node.argumentList.arguments);
-      }
-    }
+    super.visitFormalParameterList(node);
   }
 
   @override
   void visitListLiteral(ListLiteral node) {
     _visitNodeList(node.elements);
+    super.visitListLiteral(node);
   }
 
   @override
   void visitSetOrMapLiteral(SetOrMapLiteral node) {
     _visitNodeList(node.elements);
-  }
-
-  @override
-  void visitMethodInvocation(MethodInvocation node) {
-    if (node.argumentList.arguments.length > 1) {
-      _visitNodeList(node.argumentList.arguments);
-    }
+    super.visitSetOrMapLiteral(node);
   }
 
   void _visitNodeList<T extends AstNode>(NodeList<T> list) {
-    if (list.isEmpty) return;
+    if (list.length <= 1) return;
 
     if (list.last.endToken?.next?.type != TokenType.COMMA &&
         _lineOf(list.last.end) != _lineOf(list.owner.end)) {
